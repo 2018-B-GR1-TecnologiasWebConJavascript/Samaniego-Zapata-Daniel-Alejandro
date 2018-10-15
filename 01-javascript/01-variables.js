e5541b98a97211d080bc988517ec320342dd4625
Daniel Samaniego <daniel.samaniego4@hotmail.com>
2018-10-14 19:20:07 -0500

    var1

--- 01-javascript/01-variables.js
+++ 01-javascript/01-variables.js
@@ -3,11 +3,10 @@
 // NO TIPADO
 
 var edad = 10;
-edad = "10";
-var sueldo =1.235;
-edadString="10";
-
-
+var edadString = "10";
+var sueldo = 1.235;
+var casado = false;
+var hijos = null;
 var dato;
 var fechaNacimiento = new Date();
 
@@ -26,8 +25,14 @@ var daniel = {
 
 console.log("Hola Mundo");
 
-console.log("dato valor", dato); //undefined
-console.log("dato tipo", typeof dato, ); //undefined
+console.log("edad", typeof edad);           // number
+console.log('edadString', typeof edadString);     // String
+console.log('sueldo', typeof sueldo);        // number
+console.log('casado', typeof casado);       // boolean
+console.log("hijos", typeof hijos);         // object
+
+console.log("dato valor", dato);            //undefined
+console.log("dato tipo", typeof dato,);     //undefined
 console.log("fechaNacimiento", typeof fechaNacimiento); //undefinid
 
 
@@ -45,8 +50,8 @@ daniel.hija = {
 
 console.log(daniel.abuelo.nombre);  //error
 
-if(true){
+if (true) {
     console.log("Si")
-}else{
+} else {
     console.log("No")
 }
