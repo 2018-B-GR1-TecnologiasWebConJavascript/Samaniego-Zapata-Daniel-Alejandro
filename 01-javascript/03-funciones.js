9295225df11a15b4e5754e4eafe1003614a1fe98
DanSamaniego <daniel.samaniego4@hotmail.com>
2018-10-22 17:00:56 -0500

    AROOW

--- 01-javascript/03-funciones.js
+++ 01-javascript/03-funciones.js
@@ -89,3 +89,95 @@ arreglo.findIndex(
         return 2;
     }
 ); //1
+
+function restar (a, b){
+    return a-b;
+}
+
+console.log(restar(4, 2)); //Ejecucion 2
+console.log(typeof restar);    //Tipo funcion = function
+console.log(restar);  //DEFINICION DE LA FUNCION
+
+// Anonymous function
+
+function nombre(){
+
+}
+var ejemplo = function nombreDos() {};    // funcion anonima
+
+var adrian = {
+    trabajo: function(){
+        //implementacion
+    }
+};
+adrian.trabajo();
+
+var arreglo = [
+    function () {
+            //implememtacion
+    }
+];
+arreglo[0]();
+
+
+saludar("Maria", function (nombre){
+    return nombre + "Eguez"
+});
+
+
+//  TIPOS DE VARIABLES
+
+var variable; //nunca màs escribir
+
+let variablesDos = 2;   //
+variableDos = 3;    //Puedo reasignarle
+
+const edad = 29;  //No puede ser reasignada .... SIEMPRE QUE SE PUEDA USAR
+// edad = 30
+
+const vicente = {
+    nombre: 'Vicente'
+};
+vicente.nombre = 'Adrian';
+/*vicente = {
+    algo:'mas'
+};*/
+vicente.isPrototypeOf();
+vicente.hasOwnProperty();
+
+const arregloUnoDos = [1,2];
+arregloUnoDos[0] = 3;
+//arregloUnoDos = [1,2,3,4,5];
+
+const Nnombre = 'Adrian';
+//nombre = 'Vicente';
+
+const casado = true;
+//casado = false;
+
+const hijos = null;
+//hijos = 1;
+
+/*const ganarDinero = function () {
+    return 2;
+};
+*/
+
+// NUNCA VAMOS A USAR FUNCIONES ANÒNIMAS
+
+const elevarAlCuadrado = (numero) => {
+    return numero * numero;
+};
+
+//Closher es donde yo puedo usar alas variables
+
+//FAT ARROW FUNCTION   ->      =>
+//o FUNCIONES DE FLECHA GORDA   .... VARIABLES, OBJETOS,
+// PARAMETROS DE FUNCIONES Y EN UN ARREGLO SE PUEDE USAR
+//FUNCIONES ANONIMAS
+
+const elevarAlCuadradoV2 = (numero) => numero * numero;
+
+const elevarAlCuadradoV3 = numero => numero * numero;
+
+const restarDosNumeros = (numUno, numDos) => numUno - numDos;
