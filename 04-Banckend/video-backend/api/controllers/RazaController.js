16da3ab9a1a40ba3f2f2b77a6e8c84d337882c4c
Daniel Samaniego <daniel.samaniego4@hotmail.com>
2019-01-06 18:03:43 -0500

    backend

--- 04-Banckend/video-backend/api/controllers/RazaController.js
+++ 04-Banckend/video-backend/api/controllers/RazaController.js
@@ -8,12 +8,12 @@
 // http://localhost:1337/Raza/holaMundo
 // http://localhost:1337/Usuario/registrar
 
-// expressjs
+// Framework -- expressjs
 
 module.exports = {
 
   holaMundo: (peticion, respuesta)=>{
-
+  //cliente -- peticion ..... servidor -- respuesta
     return respuesta.send('ok');
   }
 
@@ -28,10 +28,14 @@ module.exports = {
 // http://localhost:1337/Raza
 // METODO HTTP: GET
 
+// Find by ...->
+// http://localhost:1337/Raza?nombre=Carlos
+// METODO HTTP: GET
+
 // Create ->
 // // http://localhost:1337/Raza
 // // METODO HTTP: POST
-// // Parametros
+// // Parametros -> Body (Formulario)
 
 // Update ->
 // http://localhost:1337/Raza/id
@@ -51,3 +55,33 @@ module.exports = {
 
 
 // Parametros
+
+// Query Params   DE CONSULTA
+// ESTAN AL FINAL
+// Se empieza con ?
+// Se separa con &
+// http://localhost:1337/Raza?nombre=Adrian&Apellido=Eguez
+// Sirve en TODOS LOS METODOS HTTP
+
+// Route Params   Cambian dinamicamente a la URL
+// Dinamicos
+// http://localhost:1337/Raza/1/casa/2/cuarto/13
+// Sirve en TODOS LOS METODOS HTTP
+
+// Form Params (Body Params)
+// Sirve en TODOS LOS METODOS HTTP EXCEPTO METODO GET
+
+// JS
+
+// <form>
+// <imput type="text" name="nombre" value="Arian">
+// <imput type="text" name="nombre" value="Eguez">
+// </form>
+
+// Codigos de Estatus -> Status Code
+
+// 1xx Informacion
+// 2xx Exito
+// 3xx Redireccion
+// 4xx Error del Cliente
+// 5xx Error de Servidor
